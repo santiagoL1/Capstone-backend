406b4dd85c1c16de6bfc440d3e2c4f260a36db84
whogenkamp3 <wjh75783@creighton.edu>
2024-11-07 17:28:03 -0600

    working version with swagger (gonna make developing the backend real nice )

--- backend/urls.py
+++ backend/urls.py
@@ -14,9 +14,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
 from django.contrib import admin
+from drf_yasg import openapi
 from django.urls import path
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Your API",
+        default_version='v1',
+        description="API documentation",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
