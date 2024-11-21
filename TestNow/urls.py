72386f58742cd82df7fcfdbb347fc19ce6c58fa3
whogenkamp3 <wjh75783@creighton.edu>
2024-11-20 18:37:45 -0600

    Update urls.py

--- TestNow/urls.py
+++ TestNow/urls.py
@@ -4,7 +4,6 @@ from .views import LoginView, UserDetailView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
-    #path('user/', UserDetailView.as_view(), name='user-detail'),
     path('user/<int:user_id>/', UserDetailView.as_view(), name='user-detail'),
 ]
 
